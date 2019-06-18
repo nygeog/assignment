4f0b78d6fccb5ae1f3dd5e75cfe94e0d2b53cfd8
DanielMSheehan <danielmsheehan@Daniels-MacBook-Pro-13.local>
2019-06-18 15:28:52 -0400

    update report notebook and assignment.py

--- assignment.py
+++ assignment.py
@@ -13,13 +13,14 @@ plt.rcParams['figure.figsize'] = [15, 5]
 
 
 def run_assignment(config):
-
     data = read_json(config)
-
     steps = data['steps']
 
-    # if "setup-workspace-retrieve-and-parse-data" in steps:
-    #     pass
+    if "run" in steps:
+        df = setup_workspace_retrieve_and_parse_data(data)
+        select_historic_data(df)
+        annualized_rate_of_return(df)
+        logistic_regression(df)
 
 
 def setup_workspace_retrieve_and_parse_data(data):
@@ -93,16 +94,10 @@ def setup_workspace_retrieve_and_parse_data(data):
         ),
     )
 
-    return 'data/processing/data_filtered.csv'
+    return df
 
 
-def select_historic_data():
-    df = pd.read_csv(
-        'data/processing/data_filtered.csv',
-        low_memory=False,
-        parse_dates=['issue_date']
-    )
-
+def select_historic_data(df):
     dfs = df[~(df.months < 36)].copy()
 
     fully_paid_count = dfs.groupby(
@@ -153,109 +148,100 @@ def select_historic_data():
 
     plt.savefig('img/top_20_default_categories.png')
 
-    if "annualized-rate-of-return" in steps:
-        df = pd.read_csv(
-            'data/processing/data_filtered.csv',
-            low_memory=False,
-            parse_dates=['issue_date']
+
+def annualized_rate_of_return(df):
+    dfs = df[~(df.months < 36)].copy()
+
+    print('    creating annualized rate of return.')
+    rate_return = dfs.groupby(
+        ['year_grade'],
+        as_index=False,
+    ).agg({'annualized_rate_return': 'mean'})
+
+    rate_return_sort = rate_return.sort_values(
+        ['annualized_rate_return'],
+        ascending=[False],
+    )
+
+    rate_return_sort.to_csv(
+        'data/processing/rate_return_sort.csv',
+        index=False,
+    )
+
+    rate_return_sort.set_index("year_grade", drop=True, inplace=True)
+
+    rate_return_top = rate_return_sort.head(10)
+    rate_return_bot = rate_return_sort.tail(10)
+
+    rate_return_top.plot.bar(rot=0)
+    plt.savefig('img/rate_return_top.png')
+
+    rate_return_bot.plot.bar(rot=0)
+    plt.savefig('img/rate_return_bot.png')
+
+
+def logistic_regression(df):
+    df = df.drop(
+        [
+            'issue_date',  # parsed out to month and year
+            'z_score_annual_inc',  # removing these, for filtering part 1
+            'z_score_revol_bal',  # removing these, for filtering part 1
+            'z_score_dti',  # removing these, for filtering part 1
+            'year_grade',  # year and grade are also own columns
+            'loan_status',  # this is dummy in 'default' col
+            'max_date',  # removing, for filtering in part 2
+            'issue_d',  # original date field, removed sim. to 'issue_date'
+        ],
+        axis=1,
+    )
+
+    one_hot = pd.get_dummies(df)
+
+    x = one_hot.drop('default', axis=1)
+    y = df[['default']]
+
+    # x.to_csv('data/processing/data_filtered_train.csv')
+
+    x_train, x_test, y_train, y_test = train_test_split(
+        x, y.values.ravel(),
+        test_size=0.3,
+        random_state=0,
+    )
+    model = LogisticRegression(solver='lbfgs', max_iter=500)
+    model.fit(x_train, y_train)
+
+    y_pred = model.predict(x_test)
+
+    print(
+        '    Accuracy of logistic regression on test: {:.2f}%'.format(
+            model.score(x_test, y_test) * 100
         )
+    )
 
-        dfs = df[~(df.months < 36)].copy()
+    confusion_matrix_plot = confusion_matrix(y_test, y_pred)
+    print('    Confusion Matrix Plot')
+    print(confusion_matrix_plot)
 
-        print('    creating annualized rate of return.')
-        rate_return = dfs.groupby(
-            ['year_grade'],
-            as_index=False,
-        ).agg({'annualized_rate_return': 'mean'})
+    print('    Classification Report')
+    print(classification_report(y_test, y_pred))
 
-        rate_return_sort = rate_return.sort_values(
-            ['annualized_rate_return'],
-            ascending=[False],
-        )
-
-        rate_return_sort.to_csv(
-            'data/processing/rate_return_sort.csv',
-            index=False,
-        )
-
-        rate_return_sort.set_index("year_grade", drop=True, inplace=True)
-
-        rate_return_top = rate_return_sort.head(10)
-        rate_return_bot = rate_return_sort.tail(10)
-
-        rate_return_top.plot.bar(rot=0)
-        plt.savefig('img/rate_return_top.png')
-
-        rate_return_bot.plot.bar(rot=0)
-        plt.savefig('img/rate_return_bot.png')
-
-    if 'logistic-regression' in steps:
-        df = pd.read_csv(
-            'data/processing/data_filtered.csv',
-            low_memory=False,
-        )
-
-        df = df.drop(
-            [
-                'issue_date',  # parsed out to month and year
-                'z_score_annual_inc',  # removing these, for filtering part 1
-                'z_score_revol_bal',  # removing these, for filtering part 1
-                'z_score_dti',  # removing these, for filtering part 1
-                'year_grade',  # year and grade are also own columns
-                'loan_status',  # this is dummy in 'default' col
-                'max_date',  # removing, for filtering in part 2
-                'issue_d',  # original date field, removed sim. to 'issue_date'
-            ],
-            axis=1,
-        )
-
-        one_hot = pd.get_dummies(df)
-
-        x = one_hot.drop('default', axis=1)
-        y = df[['default']]
-
-        # x.to_csv('data/processing/data_filtered_train.csv')
-
-        x_train, x_test, y_train, y_test = train_test_split(
-            x, y.values.ravel(),
-            test_size=0.3,
-            random_state=0,
-        )
-        model = LogisticRegression(solver='lbfgs', max_iter=500)
-        model.fit(x_train, y_train)
-
-        y_pred = model.predict(x_test)
-
-        print(
-            '    Accuracy of logistic regression on test: {:.2f}%'.format(
-                model.score(x_test, y_test) * 100
-            )
-        )
-
-        confusion_matrix_plot = confusion_matrix(y_test, y_pred)
-        print('    Confusion Matrix Plot')
-        print(confusion_matrix_plot)
-
-        print('    Classification Report')
-        print(classification_report(y_test, y_pred))
-
-        logit_roc_auc = roc_auc_score(y_test, model.predict(x_test))
-        fpr, tpr, thresholds = roc_curve(
-            y_test,
-            model.predict_proba(x_test)[:, 1],
-        )
-        plt.figure()
-        plt.plot(
-            fpr,
-            tpr,
-            label='Logistic Regression (area = {:.2f})'.format(logit_roc_auc),
-        )
-        plt.plot([0, 1], [0, 1], 'r--')
-        plt.xlim([0.0, 1.0])
-        plt.ylim([0.0, 1.05])
-        plt.xlabel('False Positive Rate')
-        plt.ylabel('True Positive Rate')
-        plt.title('Receiver operating characteristic')
-        plt.legend(loc="lower right")
-        plt.savefig('img/log_roc_split.png')
-        # plt.show()
+    logit_roc_auc = roc_auc_score(y_test, model.predict(x_test))
+    fpr, tpr, thresholds = roc_curve(
+        y_test,
+        model.predict_proba(x_test)[:, 1],
+    )
+    plt.figure()
+    plt.plot(
+        fpr,
+        tpr,
+        label='Logistic Regression (area = {:.2f})'.format(logit_roc_auc),
+    )
+    plt.plot([0, 1], [0, 1], 'r--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver operating characteristic')
+    plt.legend(loc="lower right")
+    plt.savefig('img/log_roc_split.png')
+    # plt.show()
