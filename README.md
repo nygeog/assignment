1bd4ce6818b373121d74b2b8dd1476241549533a
Danny S <danielmsheehan@iMac.local>
2019-06-17 23:49:42 -0400

    img updates and code

--- README.md
+++ README.md
@@ -5,41 +5,48 @@
       
 - [ ] Perform any necessary cleaning and aggregations to explore and better
   understand the dataset.
-    
+  - [ ] Describe the data. describe()
 
 - [ ] Describe any assumptions you made to handle null variables and outliers.
-    - [ ] Remove outliers for Annual Income
+    - [x] Remove outliers for Annual Income
+    - [x] Remove outliers for DTI
+    - [x] Total credit revolving balance (revol_bal)
 
 - [ ] Describe the distributions of the features.
     - [ ] Include two data visualizations and 
     - [ ] Two summary statistics to support these findings.
   
-#     Part 2 Business Analysis
+# Part 2 Business Analysis
 - [x] Assume a 36 month investment period for each loan, and exclude loans with less than 36 months of data available.
+
 - [ ] What percentage of loans has been fully paid?
-- [ ] When bucketed by year of origination and grade, which cohort has the
-      highest rate of defaults? Here you may assume that any loan which
-      was not fully paid had “defaulted”.
+    * **0.7562866221941765**
+
+- [ ] When bucketed by year of origination and grade, which cohort has the highest rate of defaults? Here you may assume that any loan which was not fully paid had “defaulted”.
+
 - [ ] When bucketed by year of origination and grade, what annualized rate of
       return have these loans generated on average?
+      
     * For simplicity, use the following approximation:
       `Annualized rate of return = (total_pymnt / funded_amnt) ^ (1/3) - 1`
 
 
 # Part 3 Modeling
 - [ ] Build a logistic regression model to predict loan defaults
+
 - [ ] Assume that
+
     - [ ] (i) You are given the ability to invest in each loan independently
-    - [ ] (ii) You invest immediately following loan origination and hold to
-          maturity (36 months)
-    - [ ] (iii) All loan fields that would be known upon origination are made
-          available to you.
+    
+    - [ ] (ii) You invest immediately following loan origination and hold to maturity (36 months)
+    
+    - [ ] (iii) All loan fields that would be known upon origination are made available to you.
 
 - [ ] Was the model effective? 
     - [ ] Explain how you validated your model and describe how you measure the performance of the model.
     
     
-## Fields to use. 
+## Fields to use Definitions. 
 
 * loan_amnt - The listed amount of the loan applied for by the borrower. If at some point in time, the credit department reduces the loan amount, then it will be reflected in this value.
 * funded_amnt - The total amount committed to that loan at that point in time.
@@ -49,6 +56,6 @@
 * annual_inc - The self-reported annual income provided by the borrower during registration.
 * issue_d - The month which the loan was funded
 * dti - A ratio calculated using the borrower’s total monthly debt payments on the total debt obligations, excluding mortgage and the requested LC loan, divided by the borrower’s self-reported monthly income.
-* revol_bal - Total credit revolving balanceTotal credit revolving balance
+* revol_bal - Total credit revolving balance
 * total_pymnt - Payments received to date for total amount funded
 * loan_status - Current status of the loan
--- assignment.py
+++ assignment.py
@@ -1,6 +1,8 @@
 from tools.helpers import read_json, pandas_keep_columns
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+plt.rcParams['figure.figsize'] = [15, 5]
 
 
 def run_assignment(config):
@@ -9,7 +11,7 @@ def run_assignment(config):
 
     steps = data['steps']
 
-    if 'parse-data' in steps:
+    if "parse-data" in steps:
         df = pandas_keep_columns(
             pd.read_csv(
                 data['input_data'],
@@ -22,7 +24,7 @@ def run_assignment(config):
         df['issue_year'] = df['issue_date'].dt.year
         df['max_date'] = pd.to_datetime(df.issue_date.max())
         df['months'] = (
-                (df.max_date - df.issue_date) / np.timedelta64(1, 'M')
+            (df.max_date - df.issue_date) / np.timedelta64(1, 'M')
         ).astype(int)
 
         df['year_grade'] = df.issue_year.apply(str) + '-' + df.grade
@@ -36,15 +38,41 @@ def run_assignment(config):
             np.power(df['total_pymnt'] / df['funded_amnt'], 1/3) - 1.0
         )
 
-        # MAYBE FILTER OUT ANNUAL INCOME - OUTLIERS?
+        len_pre_filter = len(df.index)
 
         df.to_csv('data/processing/data.csv', index=False)
 
+        for col in ['annual_inc', 'revol_bal', 'dti']:
+            col_mean = df[col].mean()
+            col_std = df[col].std()
 
+            df['z_score_{}'.format(col)] = df[col].apply(
+                lambda x: (x - col_mean) / col_std
+            )
 
-    if 'select-historic-data' in steps:
+            df = df[
+                (
+                    df[
+                        'z_score_{}'.format(col)
+                    ] < 3
+                ) & (
+                    df[
+                        'z_score_{}'.format(col)
+                    ] > -3
+                )
+            ].copy()
+
+        df.to_csv('data/processing/data_filtered.csv', index=False)
+
+        print(
+            'pre-filter length: {}'.format(len_pre_filter),
+            'post-filter length: {}'.format(len(df.index)),
+            'dif. of: {}'.format(len_pre_filter - len(df.index)),
+        )
+
+    if "select-historic-data" in steps:
         df = pd.read_csv(
-            'data/processing/data.csv',
+            'data/processing/data_filtered.csv',
             low_memory=False,
             parse_dates=['issue_date']
         )
@@ -63,5 +91,73 @@ def run_assignment(config):
             )
         )
 
+        dfs['count'] = 1
 
+        year_grade_default = dfs.groupby(
+            ['year_grade', 'default'],
+        ).agg({'count': 'sum'})
 
+        year_grade = dfs.groupby(['year_grade']).agg({'count': 'sum'})
+
+        grouped_year_defaults = year_grade_default.div(
+            year_grade,
+            level='year_grade',
+        ) * 100
+
+        grouped_year_defaults = grouped_year_defaults.reset_index()
+
+        defaults = grouped_year_defaults[
+            (grouped_year_defaults['default'] == 1)
+        ]
+
+        defaults.set_index("year_grade", drop=True, inplace=True)
+
+        defaults_sort = defaults.sort_values(
+            ['default', 'count'],
+            ascending=[False, False],
+        )
+
+        defaults_sort.to_csv('data/processing/defaults_sort.csv', index=False)
+
+        defaults_count = defaults_sort[['count']]
+
+        defaults_count_top_20 = defaults_count.head(20)
+
+        defaults_count_top_20.plot.bar(rot=0)
+
+        plt.savefig('img/top_20_default_categories.png')
+
+    if "annualized-rate-of-return" in steps:
+        df = pd.read_csv(
+            'data/processing/data_filtered.csv',
+            low_memory=False,
+            parse_dates=['issue_date']
+        )
+
+        dfs = df[~(df.months < 36)].copy()
+
+        rate_return = dfs.groupby(
+            ['year_grade'],
+            as_index=False,
+        ).agg({'annualized_rate_return': 'mean'})
+
+        rate_return_sort = rate_return.sort_values(
+            ['annualized_rate_return'],
+            ascending=[False],
+        )
+
+        rate_return_sort.to_csv(
+            'data/processing/rate_return_sort.csv',
+            index=False,
+        )
+
+        rate_return_sort.set_index("year_grade", drop=True, inplace=True)
+
+        rate_return_top = rate_return_sort.head(10)
+        rate_return_bot = rate_return_sort.tail(10)
+
+        rate_return_top.plot.bar(rot=0)
+        plt.savefig('img/rate_return_top.png')
+
+        rate_return_bot.plot.bar(rot=0)
+        plt.savefig('img/rate_return_bot.png')
